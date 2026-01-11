afea177728cc80126e666ebbc48d17f312fa48dd
Bidur Niroula <bidurn@Mac.lan>
2026-01-10 20:13:42 -0700

    Add frontend and backend together

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,8 +6,9 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.use(morgan('tiny'))
+app.use(express.static('dist'))
 
+app.use(morgan('tiny'))
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
